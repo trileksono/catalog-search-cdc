416fe91c30e11a50cccc432e089dd92e58fb2ee1
trileksono23 <79951102+trileksono23@users.noreply.github.com>
2026-06-30 16:53:09 +0700

    chore: initial docker file and docker compose

--- category/src/main/java/com/tleksono/category/CategoryApplication.java
+++ category/src/main/java/com/tleksono/category/CategoryApplication.java
@@ -2,8 +2,10 @@ package com.tleksono.category;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CategoryApplication {
 
 	public static void main(String[] args) {
